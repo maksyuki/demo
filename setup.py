654dca76d9cbd6a1bc888324a5caf835f7a9bad1
maksyuki <maksyuki@126.com>
2021-12-15 16:01:00 +0800

    chore: add feature

--- setup.py
+++ setup.py
@@ -13,14 +13,22 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        'Documentation':
+        'https://packaging.python.org/tutorials/distributing-packages/',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
+        'Tracker': 'https://github.com/pypa/sampleproject/issues',
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    license='MITAAAA',
+    keywords='sample setuptools ddddddddd',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires=['taichi'],
     python_requires=">=3.7,<3.10",
 )
