5b52d790838cfbcd6271a1290e088e5692d567d5
maksyuki <maksyuki@126.com>
2021-12-17 20:11:35 +0800

    chore: modify release and setup script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="example-pkg-taichi-HERE",
-    version="0.0.4",
+    version="0.0.6",
     author="taichidemo",
     author_email="taichidemo@example.com",
     description="A small example package",
