78358189c73fcfb1468ddf1915c06a29e5c17efa
maksyuki <maksyuki@126.com>
2021-12-17 19:51:15 +0800

    chore: add release action

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="example-pkg-taichi-HERE",
-    version="0.0.3-alpha",
+    version="0.0.4",
     author="taichidemo",
     author_email="taichidemo@example.com",
     description="A small example package",
